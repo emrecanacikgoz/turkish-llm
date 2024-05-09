3d4d4f94b07027bc8a6d79714ab38d297fe1c430
ecacikgoz97 <ecacikgoz97@gmail.com>
2024-05-09 11:43:08 +0300

    .

--- pretraining/data/culturax/check.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-data = np.memmap("/truba/home/eacikgoz/hamza/turkish-llm/pretraining/data/culturax/train-parquet-all.bin", dtype=np.uint16, mode='r')
-print(data.shape)
-data = np.memmap("/truba/home/eacikgoz/hamza/turkish-llm/pretraining/data/culturax/val-parquet-all.bin", dtype=np.uint16, mode='r')
-print(data.shape)
--- pretraining/data/culturax/prepare.sh
+++ pretraining/data/culturax/prepare.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=deneme-llm
-#SBATCH -p palamut-cuda     # Kuyruk adi: Uzerinde GPU olan kuyruk olmasina dikkat edin.
-#SBATCH -A proj12           # Kullanici adi
-##SBATCH -J print_gpu       # Gonderilen isin ismi
-#SBATCH -o %J.out           # Ciktinin yazilacagi dosya adi
-#SBATCH --gres=gpu:4        # Her bir sunucuda kac GPU istiyorsunuz? Kumeleri kontrol edin.
-#SBATCH -N 1                # Gorev kac node'da calisacak?
-#SBATCH -n 1                # Ayni gorevden kac adet calistirilacak?
-#SBATCH --cpus-per-task 64  # Her bir gorev kac cekirdek kullanacak? Kumeleri kontrol edin.
-#SBATCH --time=3-0:0:0      # Sure siniri koyun.
+#SBATCH --job-name= <your-job-name> # Isim tanimlayin
+#SBATCH -p <your-partition-name> # Kuyruk ismi
+#SBATCH -A <your-project-name> # Proje ismi
+#SBATCH -o %J-<your-job-name>.o%j # Cikti dosyasi
+#SBATCH --gres=gpu:8        # Kac adet GPU isteyecegini belirtin
+#SBATCH -N 1                # Kac adet node isteyecegini belirtin
+#SBATCH -n 1                # Kac adet gorev isteyecegini belirtin
+#SBATCH --cpus-per-task 128 # Kac adet CPU isteyecegini belirtin
+#SBATCH --time=3-0:0:0      # Sure siniri koyun
 
-
-echo "Setting stack size to unlimited..."
-ulimit -s unlimited
-ulimit -l unlimited
-ulimit -a
-echo
-
-eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
-source activate nanogpt
-echo 'number of processors:'$(nproc)
-nvidia-smi
+source activate <your-env-name> # Conda ortamini aktif edin
 
 python prepare.py
-
-#python check.py
 
 
